5b84263d6b9ec5495225e3c1a93d7b670e195a6c
peachdew <chinfurui@gmail.com>
2024-04-20 02:04:07 +0800

    Formatting of topic.

--- project_demo.py
+++ project_demo.py
@@ -124,9 +124,9 @@ if ('user_name' in st.session_state) and ('user_icon' in st.session_state):
         st.markdown(":orange[Topic not set, enter one on the right!]")
 
     if 'topic' in st.session_state:
-        st.markdown("## :grey[Current Topic: ]")
+        st.markdown("##### [Current Topic: ]")
         with st.container(border=True):
-            st.markdown(f"#### {st.session_state.topic}")
+            st.markdown(f"#### :yellow[{st.session_state.topic}]")
         chat_order_input = st.text_input("(Optional) Enter a conversation sequence, 0 for Tom, 1 for Mike.",
                                          placeholder="01010")
         
